1c9622e44e43cbd63faac7dcc98b58334c8c0e8f
Tyler Nex <tylernex@gmail.com>
2025-05-08 08:53:02 -0700

    Added a config section for the npcs

--- src/main/java/com/npccontactfilter/NPCContactFilterConfig.java
+++ src/main/java/com/npccontactfilter/NPCContactFilterConfig.java
@@ -3,14 +3,19 @@ package com.npccontactfilter;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("npc contact filter")
 public interface NPCContactFilterConfig extends Config
 {
+	@ConfigSection(name = "NPCs", description = "NPCs to be filtered or kept", position = 0, closedByDefault  = true)
+	String npcSection = "NPCs";
+
 	@ConfigItem(
 		keyName = "honestJimmy",
 		name = "Honest Jimmy",
 		description = "Show Honest Jimmy",
+		section = npcSection,
 		position = 1
 	)
 	default boolean honestJimmy()
@@ -22,6 +27,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "bert",
 		name = "Bert the Sandman",
 		description = "Show Bert the Sandman",
+		section = npcSection,
 		position = 2
 	)
 	default boolean bert()
@@ -33,6 +39,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "advisorGhrim",
 		name = "Advisor Ghrim",
 		description = "Show Advisor Ghrim",
+		section = npcSection,
 		position = 3
 	)
 	default boolean advisorGhrim()
@@ -44,6 +51,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "darkMage",
 		name = "Dark Mage",
 		description = "Show Dark Mage",
+		section = npcSection,
 		position = 4
 	)
 	default boolean darkMage()
@@ -55,6 +63,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "lanthus",
 		name = "Lanthus",
 		description = "Show Lanthus",
+		section = npcSection,
 		position = 5
 	)
 	default boolean lanthus()
@@ -66,6 +75,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "spria",
 		name = "Spria",
 		description = "Show Spria",
+		section = npcSection,
 		position = 6
 	)
 	default boolean spria()
@@ -77,6 +87,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "turael",
 		name = "Turael/Aya",
 		description = "Show Turael/Aya",
+		section = npcSection,
 		position = 7
 	)
 	default boolean turael()
@@ -88,6 +99,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "mazchna",
 		name = "Mazchna",
 		description = "Show Mazchna",
+		section = npcSection,
 		position = 8
 	)
 	default boolean mazchna()
@@ -99,6 +111,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "vannaka",
 		name = "Vannaka",
 		description = "Show Vannaka",
+		section = npcSection,
 		position = 9
 	)
 	default boolean vannaka()
@@ -110,6 +123,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "chaeldar",
 		name = "Chaeldar",
 		description = "Show Chaeldar",
+		section = npcSection,
 		position = 10
 	)
 	default boolean chaeldar()
@@ -121,6 +135,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "nieve",
 		name = "Nieve/Steve",
 		description = "Show Nieve/Steve",
+		section = npcSection,
 		position = 11
 	)
 	default boolean nieve()
@@ -132,6 +147,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "duradel",
 		name = "Duradel/Kuradal",
 		description = "Show Duradel/Kuradal",
+		section = npcSection,
 		position = 12
 	)
 	default boolean duradel()
@@ -143,6 +159,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "krystilia",
 		name = "Krystilia",
 		description = "Show Krystilia",
+		section = npcSection,
 		position = 13
 	)
 	default boolean krystilia()
@@ -154,6 +171,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "konar",
 		name = "Konar",
 		description = "Show Konar",
+		section = npcSection,
 		position = 14
 	)
 	default boolean konar()
@@ -165,6 +183,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "murphy",
 		name = "Murphy",
 		description = "Show Murphy",
+		section = npcSection,
 		position = 15
 	)
 	default boolean murphy()
@@ -176,6 +195,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "cyrisus",
 		name = "Cyrisus",
 		description = "Show Cyrisus",
+		section = npcSection,
 		position = 16
 	)
 	default boolean cyrisus()
@@ -187,6 +207,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "smoggy",
 		name = "Smoggy",
 		description = "Show Smoggy",
+		section = npcSection,
 		position = 17
 	)
 	default boolean smoggy()
@@ -198,6 +219,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "ginea",
 		name = "Ginea",
 		description = "Show Ginea",
+		section = npcSection,
 		position = 18
 	)
 	default boolean ginea()
@@ -209,6 +231,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "watson",
 		name = "Watson",
 		description = "Show Watson",
+		section = npcSection,
 		position = 19
 	)
 	default boolean watson()
@@ -220,6 +243,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "barbarianGuard",
 		name = "Barbarian Guard",
 		description = "Show Barbarian Guard",
+		section = npcSection,
 		position = 20
 	)
 	default boolean barbarianGuard()
@@ -231,6 +255,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "amy",
 		name = "Amy",
 		description = "Show Amy",
+		section = npcSection,
 		position = 21
 	)
 	default boolean amy()
@@ -242,6 +267,7 @@ public interface NPCContactFilterConfig extends Config
 		keyName = "random",
 		name = "Random",
 		description = "Show Random",
+		section = npcSection,
 		position = 22
 	)
 	default boolean random()
